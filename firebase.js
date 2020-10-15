9d804db3fc6bd056582d30503d6db812ccf05f2a
Tom Chambers <tom.chambers@gmail.com>
2020-10-15 10:40:49 +0100

    add cards

---
+++ firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyB_Z18dIWQkUdAKi6Id5zpc1TyOTjvEi3Y",
+  authDomain: "text-experience.firebaseapp.com",
+  databaseURL: "https://text-experience.firebaseio.com",
+  projectId: "text-experience",
+  storageBucket: "text-experience.appspot.com",
+  messagingSenderId: "257557869966",
+  appId: "1:257557869966:web:a0978b58d883c6d187a0df",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+firebase.firestore();
